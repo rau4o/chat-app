0eb44202c356fffede8959c8ff8853d5791401c8
Rauan <rkoshkinbayev@gmail.com>
2022-05-29 01:04:03 +0600

    finish chat initialization

--- src/app/features/channels/channels.module.ts
+++ src/app/features/channels/channels.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewChannelComponent } from './new-channel/new-channel.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     NewChannelComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule
   ],
   exports: [
     NewChannelComponent
--- src/app/features/channels/new-channel/new-channel.component.ts
+++ src/app/features/channels/new-channel/new-channel.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-new-channel',
   templateUrl: './new-channel.component.html',
   styleUrls: ['./new-channel.component.scss']
 })
-export class NewChannelComponent implements OnInit {
+export class NewChannelComponent {
+
+  @Output()
+  saved = new EventEmitter<string>();
+
+  channelName = new FormControl()
 
   constructor() { }
 
-  ngOnInit(): void {
+  public onCreate(): void {
+    this.saved.emit(this.channelName.value);
+    this.channelName.reset('');
   }
-
 }
